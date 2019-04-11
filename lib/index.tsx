f495541079e77ec3a30d6c313d9774a326de712e
wky <wky_0615@163.com>
2019-04-11 14:55:01 +0800

    add publish files

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,23 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from './button/button';
 import Icon from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-}
-
-ReactDOM.render(
-  <div>
-    <Button/>
-    <Icon name="alipay" onClick={fn}/>
-    <Icon name="line"/>
-    <Icon name="qq"/>
-    <Icon name="qzone"/>
-    <Icon name="skype"/>
-    <Icon name="wechat-friend"/>
-    <Icon name="wechat"/>
-    <Icon name="weibo"/>
-  </div>,
-  document.querySelector('#root')
-);
+export default Icon;
