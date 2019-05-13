2a769e031b19cbac42cd459ed5e4ebaec31ddf16
wky <wky_0615@163.com>
2019-05-13 15:54:27 +0800

    Init layout

---
+++ lib/layout/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header: React.FunctionComponent = () => {
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header;
---
+++ lib/layout/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Layout: React.FunctionComponent = () => {
+    return (
+        <div>Layout</div>
+    )
+}
+
+export default Layout;
