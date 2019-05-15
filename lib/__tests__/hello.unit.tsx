eee7482c4d906bc1a17da61ee6774fa18ee9e68f
wky <wky_0615@163.com>
2019-05-15 22:50:06 +0800

    Remove files

--- lib/__tests__/hello.unit.tsx
+++
@@ -1,5 +0,0 @@
-describe('hello', ()=>{
-  it('hello', ()=>{
-    expect(1).toEqual(1);
-  })
-})
--- lib/index.tsx
+++
@@ -1,4 +0,0 @@
-import './base/index.scss';
-import Icon from './icon/icon';
-
-export default Icon;
