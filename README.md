0e51baedcc6e6dee768c4da42be786f54e84014e
wky <wky_0615@163.com>
2019-08-21 20:50:13 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# SEELE-UI [![CircleCI](https://circleci.com/gh/wky0615/Wheels.svg?style=svg)]
-(https://circleci.com/gh/wky0615/SEELE-UI) [![npm version](https://badge.fury
-.io/js/wheels-keyu.wang.svg)](https://badge.fury.io/js/wheels-keyu.wang) [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/facebook/react/blob/master/LICENSE)
+# SEELE-UI 
+[![CircleCI](https://circleci.com/gh/wky0615/SEELE-UI/tree/master.svg?style=svg)](https://circleci.com/gh/wky0615/SEELE-UI/tree/master)
+[![npm version](https://badge.fury.io/js/seele-ui.svg)](https://badge.fury.io/js/seele-ui)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/facebook/react/blob/master/LICENSE)
--- example/doc/doc.tsx
+++ example/doc/doc.tsx
@@ -7,11 +7,11 @@ const sc = classMaker('seele')
 const DocExample = () => {
     return (<div className={sc('doc')}>
         <div className={'page-title'}>Installation, usage and customization</div>
-        <div className={'page-description'}>SEELE is a set of React components. It's powered by CSS Modules and written in TypeScript with complete defined types, and harmoniously integrates with your webpack workflow. You can take a tour through our documentation website and try the components live!</div>
+        <div className={'page-description'}>SEELE-UI is a set of React components. It's powered by CSS Modules and written in TypeScript with complete defined types, and harmoniously integrates with your webpack workflow. You can take a tour through our documentation website and try the components live!</div>
         <div className={'installation'}>
             <div className={'install-title'}>Installation</div>
             <div className={'install-description'}>
-                SEELE can be installed as an <a className={'install-link'} target={'_blank'} href={'https://www.npmjs.com/package/seele-ui'}>npm package</a>:
+                SEELE-UI can be installed as an <a className={'install-link'} target={'_blank'} href={'https://www.npmjs.com/package/seele-ui'}>npm package</a>:
             </div>
             <div className={'quote'}>$ npm install --save seele-ui</div>
         </div>
