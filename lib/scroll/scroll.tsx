4ae8c77ff05cc4d3539725f397a982b11f621317
wky <wky_0615@163.com>
2019-12-17 17:57:52 +0800

    Add scroll event for scrollbar

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {UIEventHandler, useEffect, useRef, useState} from 'react';
 import './scroll.scss';
 import classes, {classMaker} from "../helpers/classMaker";
 import scrollbarWidth from "../../example/scroll/getScrollBarWidth";
@@ -10,10 +10,28 @@ interface IProps extends IStyledProps{
 
 const Scroll: SFC<IProps> = (props) => {
     const {children, className, ...rest} = props;
+    const [barHeight, setBarHeight] = useState(0)
+    const [barTop, setBarTop] = useState(0)
+    const onScroll:UIEventHandler = (e) => {
+        const scrollHeight = e.currentTarget.scrollHeight;
+        const scrollTop = e.currentTarget.scrollTop;
+        const viewHeight = e.currentTarget.getBoundingClientRect().height;
+        setBarTop(scrollTop * viewHeight / scrollHeight);
+    }
+    const ref = useRef<HTMLDivElement>(null)
+    useEffect(()=> {
+        const scrollHeight = ref.current!.scrollHeight;
+        const viewHeight = ref.current!.getBoundingClientRect().height;
+        setBarHeight(viewHeight * viewHeight / scrollHeight);
+    }, [])
     return (
         <div {...rest} className={classes(sc(''), className)}>
-            <div className={sc('inner')} style={{right: -scrollbarWidth()}}>
+            <div className={sc('inner')} style={{right: -scrollbarWidth()}}
+            onScroll={onScroll} ref={ref}>
                 {children}
+            </div>
+            <div className={sc('track')}>
+                <div className={sc('bar')} style={{height: `${barHeight}px`, transform: `translateY(${barTop}px)`}}/>
             </div>
         </div>
     )
