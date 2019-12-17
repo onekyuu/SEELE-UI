8ef24e4c2a7c0b8322600b598815b5bc36ff9f52
wky <wky_0615@163.com>
2019-12-17 15:16:50 +0800

    Improve logic of hide scrollbar

--- example/scroll/getScrollBarWidth.tsx
+++ example/scroll/getScrollBarWidth.tsx
@@ -4,15 +4,14 @@ export default function scrollbarWidth() {
     const boxStyle = box.style;
 
     boxStyle.position = 'absolute';
-    boxStyle.top = boxStyle.left = '-99999px';
+    boxStyle.top = boxStyle.left = '300px';
     boxStyle.width = boxStyle.height = '100px';
     boxStyle.overflowX = 'scroll';
     boxStyle.overflowY = 'scroll';
-    box.classList.add('seele-hide-scrollbar');
 
     body.appendChild(box);
 
-    const width = box.offsetWidth - box.clientWidth;
+    const width = navigator.platform.indexOf('Mac') === 0 ? 16 : box.offsetWidth - box.clientWidth;
 
     body.removeChild(box);
 
