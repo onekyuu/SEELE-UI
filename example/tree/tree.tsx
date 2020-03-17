9d6031cad684a7437d4f32acbe42e8dbb33b5b41
wky <wky_0615@163.com>
2020-03-17 19:36:43 +0800

    Add selectedData

--- example/tree/tree.tsx
+++ example/tree/tree.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, {useState} from 'react';
+import {useTranslation} from 'react-i18next';
 import './tree.scss';
-import Tree from "../../lib/tree/tree";
+import Tree, {SourceItem} from "../../lib/tree/tree";
+// import Icon from "../../lib/icon/icon";
 
 const TreeExample = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
+    const [treeData] = useState([
+        {
+            key: 'item 1',
+            value: 'item 1',
+            children: [
+                {
+                    key: 'item 1.1',
+                    value: 'item 1.1',
+                    children: [
+                        {
+                            key: 'item 1.1.1',
+                            value: 'item 1.1.1',
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            key: 'item 2',
+            value: 'item 2',
+            children: [
+                {
+                    key: 'item 2.1',
+                    value: 'item 2.1',
+                },
+            ]
+        }
+    ]);
+    // const icon = <Icon name="arrow-up"/>
+    const [selectedData, setSelectedData] = useState(['item 1.1', 'item 2.1', 'item 1.1.1']);
+    const onCheckboxChange = (item: SourceItem, isChecked: boolean) => {
+        if (isChecked) {
+            setSelectedData([...selectedData, item.value])
+        } else {
+            setSelectedData(selectedData.filter(v => v !== item.value))
+        }
+    }
     return (
         <div>
             <div className={"example"}>
                 <h4 className={"tree-title"}>
                     {t('basic_tree')}
                 </h4>
-                <Tree/>
+                <div style={{width: '200px'}}>
+                    <Tree sourceData={treeData} selected={selectedData} onChange={onCheckboxChange}/>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TreeExample
+export default TreeExample;
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import './tree.scss';
 import classes, {classMaker} from "../helpers/classMaker";
-import Button from "../button/button";
 
 const componentName = 'Tree';
-const sc = classMaker('seele-message');
+const sc = classMaker('seele-tree');
 
-interface IProps extends IStyledProps {
+export interface SourceItem {
+    key: string,
+    value: string,
+    children?: SourceItem[],
 }
 
-const Tree: SFC<IProps> = () => {
+interface IProps extends IStyledProps {
+    sourceData: SourceItem[],
+    icon?: ReactElement,
+    selected: string[],
+    onChange: (item: SourceItem, isChecked: boolean) => void,
+}
+
+const Tree: SFC<IProps> = (props) => {
+    const {sourceData, icon, selected, onChange} = props;
+    const renderItem = (item: SourceItem,
+                        selected: string[],
+                        onChange: (item: SourceItem, isChecked: boolean) => void,
+                        level = 0) => {
+        const classList = {
+            [`level-${level}`]: true,
+            ['item']: true,
+        };
+        return <div className={sc(classList)} key={item.value}>
+            <div className={sc('text')}>
+                {icon}
+                <input className={sc('checkbox')}
+                       type="checkbox"
+                       onChange={(e) => onChange(item, e.target.checked)}
+                       checked={selected.indexOf(item.value) >= 0}/>
+                {item.key}
+            </div>
+            {item.children?.map((subItem: SourceItem) => {
+                return renderItem(subItem, selected, onChange, level + 1);
+            })}
+        </div>;
+    };
     return (
         <div className={classes(sc(''))}>
-            <Button>tree</Button>
+            {sourceData.map(item => {
+                return renderItem(item, selected, onChange);
+            })}
         </div>
-    )
+    );
 };
 
 Tree.displayName = componentName;
-Tree.defaultProps = {
-};
+Tree.defaultProps = {};
 export default Tree;
