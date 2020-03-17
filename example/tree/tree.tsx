e07e352a9be62b3b20d353bc9da2108e00a6a516
wky <wky_0615@163.com>
2020-03-17 19:54:20 +0800

    Add multiple

--- example/tree/tree.tsx
+++ example/tree/tree.tsx
@@ -9,15 +9,15 @@ const TreeExample = () => {
     const [treeData] = useState([
         {
             key: 'item 1',
-            value: 'item 1',
+            value: '1',
             children: [
                 {
                     key: 'item 1.1',
-                    value: 'item 1.1',
+                    value: '1.1',
                     children: [
                         {
                             key: 'item 1.1.1',
-                            value: 'item 1.1.1',
+                            value: '1.1.1',
                         },
                     ]
                 },
@@ -25,24 +25,26 @@ const TreeExample = () => {
         },
         {
             key: 'item 2',
-            value: 'item 2',
+            value: '2',
             children: [
                 {
                     key: 'item 2.1',
-                    value: 'item 2.1',
+                    value: '2.1',
                 },
             ]
         }
     ]);
     // const icon = <Icon name="arrow-up"/>
-    const [selectedData, setSelectedData] = useState(['item 1.1', 'item 2.1', 'item 1.1.1']);
+    const [selectedData, setSelectedData] = useState(['1.1', '2.1',
+        '1.1.1']);
+    const [selected] = useState('1.1.1')
     const onCheckboxChange = (item: SourceItem, isChecked: boolean) => {
         if (isChecked) {
-            setSelectedData([...selectedData, item.value])
+            setSelectedData([...selectedData, item.value]);
         } else {
-            setSelectedData(selectedData.filter(v => v !== item.value))
+            setSelectedData(selectedData.filter(v => v !== item.value));
         }
-    }
+    };
     return (
         <div>
             <div className={"example"}>
@@ -50,7 +52,8 @@ const TreeExample = () => {
                     {t('basic_tree')}
                 </h4>
                 <div style={{width: '200px'}}>
-                    <Tree sourceData={treeData} selected={selectedData} onChange={onCheckboxChange}/>
+                    <Tree sourceData={treeData} selected={selected}
+                          onChange={onCheckboxChange}/>
                 </div>
             </div>
         </div>
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -11,41 +11,48 @@ export interface SourceItem {
     children?: SourceItem[],
 }
 
-interface IProps extends IStyledProps {
-    sourceData: SourceItem[],
-    icon?: ReactElement,
+type multiType = {
     selected: string[],
-    onChange: (item: SourceItem, isChecked: boolean) => void,
+    multiple: true,
+}
+type singleType = {
+    selected: string,
+    multiple?: false,
 }
 
+type IProps = {
+    sourceData: SourceItem[],
+    icon?: ReactElement,
+    onChange: (item: SourceItem, isChecked: boolean) => void,
+} & (multiType | singleType)
+
 const Tree: SFC<IProps> = (props) => {
-    const {sourceData, icon, selected, onChange} = props;
-    const renderItem = (item: SourceItem,
-                        selected: string[],
-                        onChange: (item: SourceItem, isChecked: boolean) => void,
-                        level = 0) => {
+    const {sourceData, icon, selected, onChange, multiple} = props;
+    const renderItem = (item: SourceItem, level = 0) => {
         const classList = {
             [`level-${level}`]: true,
             ['item']: true,
         };
+        const checked = multiple ? selected.indexOf(item.value) >= 0 :
+            selected === item.value;
         return <div className={sc(classList)} key={item.value}>
             <div className={sc('text')}>
                 {icon}
                 <input className={sc('checkbox')}
                        type="checkbox"
                        onChange={(e) => onChange(item, e.target.checked)}
-                       checked={selected.indexOf(item.value) >= 0}/>
+                       checked={checked}/>
                 {item.key}
             </div>
             {item.children?.map((subItem: SourceItem) => {
-                return renderItem(subItem, selected, onChange, level + 1);
+                return renderItem(subItem, level + 1);
             })}
         </div>;
     };
     return (
         <div className={classes(sc(''))}>
             {sourceData.map(item => {
-                return renderItem(item, selected, onChange);
+                return renderItem(item);
             })}
         </div>
     );
