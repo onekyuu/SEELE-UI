c0ceca3d17504bb09f6919a0ac16afaa7db07d77
wky <wky_0615@163.com>
2020-03-18 17:25:52 +0800

    Add animate of collapse

--- lib/tree/tree-item.tsx
+++ lib/tree/tree-item.tsx
@@ -1,10 +1,12 @@
-import React, {ChangeEventHandler, useState} from "react";
+import React, {ChangeEventHandler, useRef, useState} from "react";
 import Icon from "../icon/icon";
 import {classMaker} from "../helpers/classMaker";
 import {SourceItem, TreeProps} from "./SourceItem";
 import useUpdate from "../hooks/useUpdate";
+import './tree.scss';
 
 const sc = classMaker('seele-tree');
+
 interface Props {
     item: SourceItem;
     level: number;
@@ -17,7 +19,8 @@ const TreeItem: React.FC<Props> = (props) => {
         [`level-${level}`]: true,
         ['item']: true,
     };
-    const checked = treeProps.multiple ? treeProps.selected.indexOf(item.value) >= 0 :
+    const checked = treeProps.multiple ?
+        treeProps.selected.indexOf(item.value) >= 0 :
         treeProps.selected === item.value;
     const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
         if (treeProps.multiple) {
@@ -32,9 +35,44 @@ const TreeItem: React.FC<Props> = (props) => {
         }
     };
     const [collapse, setCollapse] = useState(false);
-    useUpdate(collapse, () => {console.log(1)})
+    const itemRef = useRef<HTMLDivElement>(null);
+    useUpdate(collapse, () => {
+        if (!itemRef.current) return
+        if (collapse) {
+            const {height} = itemRef.current.getBoundingClientRect();
+            itemRef.current.style.height = height + 'px';
+            itemRef.current.getBoundingClientRect();
+            itemRef.current.style.height = '0px';
+            const close = () => {
+                if (itemRef.current) {
+                    itemRef.current.style.height = '';
+                    itemRef.current.classList.add('close');
+                    itemRef.current.removeEventListener('transitionend', close);
+                }
+            };
+            itemRef.current.addEventListener('transitionend', close);
+        } else {
+            itemRef.current.style.position = 'absolute';
+            itemRef.current.style.opacity = '0';
+            itemRef.current.style.height = 'auto';
+            const {height} = itemRef.current.getBoundingClientRect();
+            itemRef.current.style.position = '';
+            itemRef.current.style.opacity = '';
+            itemRef.current.style.height = '0px';
+            itemRef.current.getBoundingClientRect();
+            itemRef.current.style.height = height + 'px';
+            const open = () => {
+                if (itemRef.current) {
+                    itemRef.current.style.height = '';
+                    itemRef.current.classList.add('open');
+                    itemRef.current.removeEventListener('transitionend', open);
+                }
+            };
+            itemRef.current.addEventListener('transitionend', open);
+        }
+    });
     return <div className={sc(classList)} key={item.value}>
-        <div className={sc('text')}  onClick={() => setCollapse(!collapse)}>
+        <div className={sc('text')} onClick={() => setCollapse(!collapse)}>
             {item.children && <span>
                     {collapse ? <Icon name="arrow-right"/> :
                         <Icon name="arrow-down"/>
@@ -47,9 +85,10 @@ const TreeItem: React.FC<Props> = (props) => {
                    checked={checked}/>
             {item.key}
         </div>
-        <div className={sc({subItem: true, collapsed: collapse})}>
+        <div className={sc({subItem: true, collapsed: collapse})} ref={itemRef}>
             {item.children?.map((subItem: SourceItem) => {
-                return <TreeItem item={subItem} level={level + 1} treeProps={treeProps} key={subItem.value}/>;
+                return <TreeItem item={subItem} level={level + 1}
+                                 treeProps={treeProps} key={subItem.value}/>;
             })}
         </div>
     </div>;
