25c25f36ad898284b6e3383fa75d74eb53d1bed3
keyu.wang <keyu.wang@arkie.ai>
2021-07-04 16:56:45 +0800

    add message component example

---
+++ example/message/message.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from "../../lib/button/button";
+
+const MessageExample: React.FunctionComponent = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={'messageExample'}>
+            <h4>{t('basic_usage')}</h4>
+            <div className={'example-container'}>
+                <Button>点击</Button>
+            </div>
+        </div>
+    );
+}
+
+export default MessageExample;
