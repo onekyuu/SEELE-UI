60f0f1ec828507f73c049b760580b490fe997a31
keyu <me@keyu.email>
2022-11-27 19:27:08 +0800

    feat: add switch component

---
+++ example/switch/switch.demo.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { useTranslation } from 'react-i18next';
+
+const SwitchDemo = () => {
+  const {t} = useTranslation()
+  return <div></div>
+}
+
+export default SwitchDemo
